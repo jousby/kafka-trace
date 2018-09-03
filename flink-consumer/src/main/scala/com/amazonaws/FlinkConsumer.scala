b9e23ba88af217b9fee1852c26dcb6955019e5bd
James Ousby <jousby@gmail.com>
2018-09-03 21:18:53 +1000

    Completed simple consumer and per second logging.

--- flink-consumer/src/main/scala/com/amazonaws/FlinkConsumer.scala
+++ flink-consumer/src/main/scala/com/amazonaws/FlinkConsumer.scala
@@ -1,5 +1,6 @@
 package com.amazonaws
 
+import java.time.Instant
 import java.util.Properties
 
 import com.amazonaws.model.Transaction
@@ -37,21 +38,49 @@ object FlinkConsumer extends LazyLogging {
     consumerProps.put("group.id", "txns-consumer-group")
     consumerProps.put("specific.avro.reader", "true")
 
+    var stopping = false
     //create new consumer
     val consumer = new  KafkaConsumer[String, Transaction](consumerProps)
 
     logger.info("About to start consuming messages from Kafka - Press CTRL-C to terminate.")
-    Runtime.getRuntime.addShutdownHook(new Thread(() => consumer.close()))
+    Runtime.getRuntime.addShutdownHook(new Thread(() => {
+      stopping = true
+      consumer.close()
+    }))
 
     //subscribe to producer's topic
     consumer.subscribe(java.util.Arrays.asList("txns"))
 
+    var msgCounter = 0
+
+    // attempts to report metrics once per second
+    val metricsObvserver = new Thread() {
+      override def run(): Unit = {
+        logger.info(s"TrafficSimulator target rate set to unlimited")
+        var msgWatermark = msgCounter
+        var targetSecond = Instant.now.getEpochSecond + 1
+        while (!stopping) {
+          // sleep until the next reporting second
+          while (Instant.now.getEpochSecond < targetSecond) Thread.sleep(1)
+
+          // second has ticked over lets report
+          val msgCounterSnap = msgCounter
+          logger.info(s"Current tps ${msgCounterSnap - msgWatermark}")
+          msgWatermark = msgCounterSnap
+          targetSecond = targetSecond + 1
+        }
+      }
+    }
+
+    metricsObvserver.start()
+
     //poll for new messages every two seconds
-    while(true) {
+    while(!stopping) {
       val records = consumer.poll(500)
+      msgCounter += records.count()
 
       //print each received record
-      logger.debug(s"Fetched ${records.count()} records")
+      //logger.debug(s"Fetched ${records.count()} records")
 
       //commit offsets on last poll
       consumer.commitSync()
--- traffic-simulator/src/main/scala/com/amazonaws/TrafficSimulator.scala
+++ traffic-simulator/src/main/scala/com/amazonaws/TrafficSimulator.scala
@@ -16,31 +16,83 @@ trait MessageGenerator[T] {
   def create(): T
 }
 
-class TrafficSimulator[T](producerProps: Properties, topic: String, messageGenerator: MessageGenerator[T]) extends LazyLogging {
+class TrafficSimulator[T](producerProps: Properties, topic: String, messageGenerator: MessageGenerator[T], targetTps: Int = 0) extends LazyLogging {
 
   lazy val producer = new KafkaProducer[String, T](producerProps)
   var stopping = false
+  val MILLIS_IN_SECOND = 1000
+  val UNLIMITED = 0
 
-  def start() = {
-    var msgCounter = 0
-    var lastReportingTime = Instant.now()
-
-    while (true && !stopping) {
-      producer.send(new ProducerRecord(topic, messageGenerator.create()))
-      msgCounter += 1
-      if (msgCounter % 1000000 == 0) {
-        val now = Instant.now()
-        val millis = ChronoUnit.MILLIS.between(lastReportingTime, now)
-        lastReportingTime = now
-        logger.debug(s"Published 1M mesg in $millis millis")
-      }
-    }
-  }
+  def start() = if (targetTps == UNLIMITED) unlimitedTraffic() else rateLimitedTraffic(targetTps)
 
   def stop() = {
     logger.info(s"Stopping Traffic Simulator for topic: $topic")
     stopping = true
     producer.close()
+  }
+
+  def rateLimitedTraffic(targetTps: Int): Unit = {
+    logger.info(s"TrafficSimulator target rate set to $targetTps per second")
+    var msgCounter = 0
+    var lastReportingTime = Instant.now()
+
+    while (!stopping) {
+      producer.send(new ProducerRecord(topic, messageGenerator.create()))
+      msgCounter += 1
+      if (msgCounter == targetTps) {
+        val now = Instant.now()
+        val elapsedMillis = ChronoUnit.MILLIS.between(lastReportingTime, now)
+        val secondDelta = MILLIS_IN_SECOND - elapsedMillis
+        val messagesPerSecond: Long = (msgCounter * (MILLIS_IN_SECOND.toDouble / elapsedMillis.toDouble)).toLong
+        lastReportingTime = now
+        msgCounter = 0
+
+        if (secondDelta > 0) {
+          logger.debug(s"Estimated tps is $messagesPerSecond, rate limiting to the target tps of $targetTps")
+          val targetSecond = now.getEpochSecond + 1
+
+          // Sleep until the next second
+          while (Instant.now.getEpochSecond < targetSecond) Thread.sleep(1)
+          lastReportingTime = Instant.now()
+        }
+        else {
+          logger.warn(s"Current tps is $messagesPerSecond, unable to achieve the target tps of $targetTps")
+        }
+      }
+    }
+    logger.info(s"$msgCounter messages sent")
+  }
+
+  def unlimitedTraffic(): Unit = {
+    var msgCounter = 0
+
+    // attempts to report metrics once per second
+    val metricsObvserver = new Thread() {
+      override def run(): Unit = {
+        logger.info(s"TrafficSimulator target rate set to unlimited")
+        var msgWatermark = msgCounter
+        var targetSecond = Instant.now.getEpochSecond + 1
+        while (!stopping) {
+          // sleep until the next reporting second
+          while (Instant.now.getEpochSecond < targetSecond) Thread.sleep(1)
+
+          // second has ticked over lets report
+          val msgCounterSnap = msgCounter
+          logger.info(s"Current tps ${msgCounterSnap - msgWatermark}")
+          msgWatermark = msgCounterSnap
+          targetSecond = targetSecond + 1
+        }
+      }
+    }
+
+    metricsObvserver.start()
+
+    while (!stopping) {
+      producer.send(new ProducerRecord(topic, messageGenerator.create()))
+      msgCounter += 1
+    }
+
+    logger.info(s"$msgCounter messages sent")
   }
 }
 
@@ -50,6 +102,7 @@ object StartTrafficSimulator {
   private class Args(args: Array[String]) extends ScallopConf(args) {
     val kafkaUrl: ScallopOption[String] = opt[String](required = true, default = Some("http://localhost:9092"))
     val registryUrl: ScallopOption[String] = opt[String](required = true, default = Some("http://localhost:8081"))
+    val targetTps: ScallopOption[Int] = opt[Int](required = true, default = Some(0))
     verify()
   }
 
@@ -64,7 +117,7 @@ object StartTrafficSimulator {
     producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, cl.kafkaUrl.toOption.get)
     producerProps.put("schema.registry.url", cl.registryUrl.toOption.get)
 
-    val txnSimulator = new TrafficSimulator[Transaction](producerProps, "txns", TransactionGenerator)
+    val txnSimulator = new TrafficSimulator[Transaction](producerProps, "txns", TransactionGenerator, cl.targetTps.toOption.get)
 
     println("About to start publishing messages to Kafka - Press CTRL-C to terminate.")
     Runtime.getRuntime.addShutdownHook(new Thread(() => txnSimulator.stop()))
